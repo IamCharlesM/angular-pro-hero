638b8ab58b3e2aed9250a01c2a176cb2fc8ee9e3
iamcharlesm <because@charlesis.me>
2018-09-04 04:04:11 -0500

    Hero can now be selected to view details, Master and Detail view created.

---
+++ pro-heros/src/app/mock-heros.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Eraser Head'},
+    { id: 12, name: 'Endeavor'},
+    { id: 13, name: 'Midnight'},
+    { id: 14, name: 'Gang Orca'},
+    { id: 15, name: 'Hawks'},
+    { id: 16, name: 'Ragdoll'},
+    { id: 17, name: 'Best Jeanist'},
+    { id: 18, name: 'Present Mic'},
+    { id: 19, name: 'Nighteye'},
+    { id: 20, name: 'Mt.Lady'}
+]
